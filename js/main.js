0cd3f0cce5c49720c2f1cefbd782e514fccc093d
Mads <mmoigaar@stud.ntnu.no>
2018-03-11 15:11:21 +0100

    filterfunksjonalitet

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-<!-- JavaScript -->
+$(document).ready(function(){
+    var selected = [];
+    var filterItems = $('.filter_item');
+    
+    $('.category_item').click(function(){
+        var checkboxes = $('[name=filterData]');
+        var selected = [];
+        filterItems.addClass('hide');
+        
+        for (var i=0; i<checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                selected.push(checkboxes[i].value);
+            }
+        }
+        for (var j=0; j<selected.length; j++) {
+            $('.' + selected[j]).removeClass('hide');
+            }
+        if(selected.length == 0){
+                filterItems.removeClass('hide');
+        }
+
+    });
+});
