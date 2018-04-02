b7609b4b8f19e09c29b969b7eb31fb8bd25fe357
ragnals <ragnals@stud.ntnu.no>
2018-04-02 13:26:36 +0200

    Hyttegrend fix

--- js/main.js
+++ js/main.js
@@ -74,30 +74,30 @@ $(document).ready(function(){
       fallImages();
     }
     function winterImages() {
-      $("#slide1").attr("src","../images/seasonal_slider_images/seasonal_vinter1.png");
-      $("#slide2").attr("src","../images/seasonal_slider_images/seasonal_vinter2.png");
-      $("#slide3").attr("src","../images/seasonal_slider_images/seasonal_vinter3.png");
+      $("#slide1").attr("src","images/seasonal_slider_images/seasonal_vinter1.png");
+      $("#slide2").attr("src","images/seasonal_slider_images/seasonal_vinter2.png");
+      $("#slide3").attr("src","images/seasonal_slider_images/seasonal_vinter3.png");
       $('#winterBtn').attr("class","btn-info");
 	    $("#springBtn, #summerBtn, #fallBtn").attr("class","btn-primary");
     }
     function springImages() {
-      $("#slide1").attr("src","../images/seasonal_slider_images/seasonal_spring1.jpg");
-      $("#slide2").attr("src","../images/seasonal_slider_images/seasonal_spring2.jpg");
-      $("#slide3").attr("src","../images/seasonal_slider_images/seasonal_spring3.jpg");
+      $("#slide1").attr("src","images/seasonal_slider_images/seasonal_spring1.jpg");
+      $("#slide2").attr("src","images/seasonal_slider_images/seasonal_spring2.jpg");
+      $("#slide3").attr("src","images/seasonal_slider_images/seasonal_spring3.jpg");
       $('#springBtn').attr("class","btn-info");
 	    $("#summerBtn, #winterBtn, #fallBtn").attr("class","btn-primary");
     }
     function summerImages() {
-      $("#slide1").attr("src","../images/seasonal_slider_images/seasonal_summer1.jpeg");
-      $("#slide2").attr("src","../images/seasonal_slider_images/seasonal_summer2.jpeg");
-      $("#slide3").attr("src","../images/seasonal_slider_images/seasonal_summer3.jpeg");
+      $("#slide1").attr("src","images/seasonal_slider_images/seasonal_summer1.jpeg");
+      $("#slide2").attr("src","images/seasonal_slider_images/seasonal_summer2.jpeg");
+      $("#slide3").attr("src","images/seasonal_slider_images/seasonal_summer3.jpeg");
       $('#summerBtn').attr("class","btn-info");
       $("#springBtn, #winterBtn, #fallBtn").attr("class","btn-primary");
     }
     function fallImages() {
-      $("#slide1").attr("src","../images/seasonal_slider_images/seasonal_fall1.jpg");
-      $("#slide2").attr("src","../images/seasonal_slider_images/seasonal_fall2.jpg");
-      $("#slide3").attr("src","../images/seasonal_slider_images/seasonal_fall3.jpg");
+      $("#slide1").attr("src","images/seasonal_slider_images/seasonal_fall1.jpg");
+      $("#slide2").attr("src","images/seasonal_slider_images/seasonal_fall2.jpg");
+      $("#slide3").attr("src","images/seasonal_slider_images/seasonal_fall3.jpg");
       $('#fallBtn').attr("class","btn-info");
       $("#springBtn, #winterBtn, #summerBtn").attr("class","btn-primary");
     }
