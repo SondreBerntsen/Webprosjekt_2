6fc90aa3336e34147d440fd717703f81336dc757
mads <mmoigaar@stud.ntnu.no>
2018-04-03 13:37:54 +0200

    Added propertyInfo to json file

---
+++ temp.json.js
@@ -0,0 +1,94 @@
+var blabla = {
+    "grendnavn": {
+        "imgHeader": "",
+        "title": "Låvåshaugen panorama",
+        "oneliner": "Hyttefeltet ligger på over 1000 moh med panoramautsikt over hele Rondane og Jotunheimen!",
+        "description": [
+            "Låvåshaugen Panorama på Kvamsfjellet er et hyttefelt på over 1000 moh med panoramautsikt over hele Rondane og Jotunheimen!",
+            "Helt fantastisk beliggenhet som bare må oppleves!",
+            "Ca 3,5 t kjøring fra Oslo og Trondheim. Fra Lillehammer ca 1,5 t. Ny E6 vil stå ferdig i 2016, noe som fører til at kjøretid fra sør vil reduseres! Helårsvei, strøm, privat vann, offentlig avløpsnett. Naturopplevelsene er mange både sommer og vinter, med ski- og turløypetrase gjennom hele feltet."
+        ],
+        "icons": [
+            "avløp",
+            "strøm",
+            "vann",
+            "vei",
+            "bading",
+            "fisking",
+            "hotell",
+            "jakt",
+            "løypenett",
+            "sykkel",
+            "turterreng"
+        ],
+        "mapdata": [
+
+        ],
+        "propertyInfo": [
+            "Solrike utsiktstomter i vestvendt, svakt skrånende terreng. Tomtene er ikke endelig oppmålt. 122 daa fordelt på 25 tomter. Tomtestørrelser fra 1,2 mål til 1,9 mål. Tomtene blir utskilt som eiertomter. Tomten er opparbeidet med gruset adkomst og plantert for grøntarealer. Gode parkeringsmuligheter utenfor hver hytte.",
+            "Av andre fasiliteter kan det opplyses at det skal anlegges en småbåthavn til de 25 fritidseiendommene. I tillegg blir det 10 stk. gjesteplasser. En båtplass kan leies eller kjøpes. Prisene er ikke fastsatt ennå med kjøpsprisen vil ligge i størrelsesorden kr 27.000,–.",
+            "På tomt 8 har Tinde prosjektert en flott hytte med gode løsninger til kun kr 2 190 000,–. Ta gjerne kontakt for mer informasjon.",
+            "Finn.no kode 79177617"
+        ],
+        "information": {
+            "arrival": [
+                "Følg E6 til Kvam sentrum og ta av til Kvamsfjellet. Følg fylkesvei 419 ca 10km og ta av ved skilt merket Låvåshaugen panorama"
+            ],
+            "facts": [
+                "MOH: Ca 1000m",
+                "Tomtestørrelse: 1800-4300m2",
+                "Pris fra: 690 000,-",
+                "Fra Oslo: 275 km",
+                "Fra Lillehammer: 92 km",
+                "Fra Trondheim: 270 km",
+                "Nærmeste butikk: 7 km",
+                "Vann: Offentlig vann",
+                "Avløp: Offentlig avløp",
+                "Strøm: Jordkabel til tomtegrense"
+            ],
+            "priceInfo": [
+
+            ],
+            "contactInfo": {
+                "name": "",
+                "tlf": "",
+                "email": ""
+            }
+        }
+
+    },
+    "grendnavn": {
+        "imgHeader": "",
+        "title": "",
+        "oneliner": "",
+        "description": [
+            "",
+            "",
+        ],
+        "icons": [
+            "",
+            "" 
+        ],
+        "mapdata": [
+
+        ],
+        "information": {
+            "arrival": [
+
+            ],
+            "facts": [
+                "",
+                ""
+            ],
+            "priceInfo": [
+
+            ],
+            "contactInfo": {
+                "name": "",
+                "tlf": "",
+                "email": ""
+            }
+        }
+
+    }
+};
