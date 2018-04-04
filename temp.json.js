ccf8d2f05a8943dfea2c35d2fc3aa00156998049
mads <mmoigaar@stud.ntnu.no>
2018-04-04 14:58:29 +0200

    More property areas in temp file

--- temp.json.js
+++ temp.json.js
@@ -126,11 +126,168 @@ var blabla = {
               "Det vises til egen prisliste for ledige tomter og tomtepriser.",
               "Pris er fra kr 545.000,–. Det tas forbehold om tomtestørrelse da endelig oppmåling ikke er foretatt. I tillegg til tomtepris kommer kommunale avgifter og et tillegg på kr 42.600,– som anleggsbidrag til kommunen for oppgradering av vanntilførsel. Oppmålingskostnader er inkludert."
             ],
-            "contactInfo": {
+            "contactInfo": [
+              {
                 "name": "TORE RUGSVEEN",
                 "tlf": "457 77 188",
                 "email": "tore.rugsveen@tindeutvikling.no"
-            }
+              }
+            ]
+        }
+
+    },
+    "tindegrenda": {
+        "imgHeader": "images/hyttegrender/tindegrenda.jpeg",
+        "title": "Tindegrenda Varden",
+        "oneliner": "Arkitekttegnede hytter i Varden!",
+        "description": [
+            "45 minutter nord for Lillehammer ligger Kvitfjell, skientusiastenes vinterparadis.",
+            "Her finner du utfordringer og skiglede for alle nivåer; enten du vil bryne deg på den olympiske utforløypa eller om du ønsker å legge dine første skiopplevelser i et av våre familieområder. Vestsiden er et perfekt utgangspunkt for barnefamilier; her er det enkel adkomst fra E6 og nytt servicebygg med, skikafé, skiutleie og sportsbutikk. ",
+            "Kvitfjell er et komplett og snøsikkert skianlegg med skiutfordringer i alle nivåer!",
+            "Anlegget er gjerne det første alpinsenteret til å åpne løypene om høsten og av de siste som stenger i slutten av april. Kvitfjell ble kåret av Norges største avis, VG, til landets beste alpinanlegg for alpinister. Nøkkelen til suksessen er å tilby skiopplevelser på øverste hylle. Over 100 snøkanoner og det samme crewet som preparerer til de årlige World Cup-rennene, sørger for at du alltid kan glede deg til å føle perfekt, nypreppet snø under skiene.",
+            "Har du lyst til å la alpinskiene hvile kan du nyte unik natur og velge fritt i 600 km preparerte langrennsløyper i den norske fjellheimen.",
+            "Les mer på: <a href='http://www.kvitfjell.no'>www.kvitfjell.no</a>"
+        ],
+        "icons": [
+            "avløp",
+            "strøm",
+            "vann",
+            "vei",
+            "fisking",
+            "hotell",
+            "jakt",
+            "løypenett",
+            "sykkel",
+            "turterreng"
+        ],
+        "mapdata": [
+
+        ],
+        "information": {
+            "propertyInfo": [
+              "Tindegrenda ligger høyt og flott til i Varden i Kvitfjell ca 850 moh. Varden ligger i Kvitfjell Vest og er den tredje fjellsiden som bygges ut i Kvitfjell. Dette er et eksklusivt hytteområde og er et supert utgangspunkt for fritidsopplevelser både sommer og vinter.",
+              "Varden får Ny stolheis, ny tilførselsgondol og 2 nye løyper ferdigstilles sesongen 17/18. Varden blir Kvitfjells familievennlige fjellside med brede, flotte grønne og blå løyper. Skiproduktet blir perfekt for hele familien.",
+              "Spenn på deg skiene og gå en tur mot Svinslåa, eller rundt om Skeikampen. Skituren kan avsluttes med en matbit og noe godt i glasset på Tyrihanstunet for de det passer for.",
+              "Kvitfjell Vest er et sted for alle. Sommerstid byr området på fine turmuligheter både med og uten sykkel.",
+              "Tindegrenda ligger pent til med utsikt mot Kvitfjell Vest og mot Gudbrandsdalen."
+              //Fix, There is more info underneath this, but it includes images. It requires restructuring of the json file if we want to include these.
+            ],
+            "arrival": [
+              "Ta av fra E6 ved Fåvang og følg skilting mot Kvitfjell Vest. Vegen slynger seg oppover i dalsiden mot Kvitfjell Vest. Etter å ha passert Servicesenteret (ligger på høyre side av vegen) tar man til venstre opp mot  TINDEGRENDA VARDEN. Det er godt skiltet. Følg denne vegen i 2,2 km og det står igjen skilt merket med Tindegrenda inn på venstre side."
+            ],
+            "facts": [
+                "600 km Langrennsløyper",
+                "34 km alpinløyper",
+                "2 Terrengparker",
+                "2 KIDS familieområder",
+                "25 Løyper, 12 blå, 6 grønne, 6 røde, 3 sorte",
+                "14 heiser; 4 stolheiser, 1 gondol, 2 T-kroker, 4 tallerkenheiser, 3 rullebånd",
+                "Toppstasjon: 1039 m",
+                "Høydemeter: 854",
+                "2,6 km flombelyste alpinløyper"
+            ],
+            "priceInfo": [
+              "<b>PRISER OG HYTTETYPER</b>",
+              "Hyttetypene det kan velges i er: VARDEN K1, VARDEN K3 og VARDEN A2.",
+              "Pris komplett med nøkkelferdig leveranse fra kr 3.850.000,- inkl. mva.",
+              "For mer info om hyttetypene, se: <a href='http://tindehytter.no'>tindehytter.no</a>"
+            ],
+            "contactInfo": [
+              {
+                "name": "TORE RUGSVEEN",
+                "tlf": "tore.rugsveen@tindeutvikling.no",
+                "email": "457 77 188"
+              }
+            ]
+            //Fix, add prospect things
+        }
+
+    },
+    "lillehammer": {
+        "imgHeader": "images/hyttegrender/lillehammer.jpeg",
+        "title": "Lillehammer Sæter III",
+        "oneliner": "Lillehammerfjellets flotteste hyttetomter selges nå på snøsikre Nordseter!",
+        "description": [
+            "Lillehammer Sæter III hyttegrend ligger på Nordseter i Lillehammer kommune. Området ligger sentralt på Nordseter like ved Landtjernet og med umiddelbar nærhet til fjell- og skogsterreng.",
+            "Her finnes milevis med skiløyper på vinterstid, og merkede turløyper om sommeren!",
+            "Nordseter ligger strategisk til midt mellom Sjusjøen i sørøstlig retning og Hafjell/Øyerfjellet i nordvestlig retning. Løypenettet på området henger nært sammen med disse destinasjonene og du kan gå på ski eller til fots ned på OL-stadion fra 1994 og Ungdoms-OL i 2016.",
+            "Nordseter er et attraktivt ferie. og rekreasjonsområde hele året. Sentrum av Lillehammer når du på ca. 12 minutter med bil (15 km)."
+        ],
+        "icons": [
+            "avløp",
+            "strøm",
+            "vann",
+            "vei",
+            "bading",
+            "butik",
+            "fisking",
+            "hotell",
+            "jakt",
+            "løypenett",
+            "sykkel",
+            "turterreng"
+        ],
+        "mapdata": [
+
+        ],
+        "information": {
+            "propertyInfo": [
+              "<b>TOMTENE</b>",
+              "Tomtene varier i størrelse fra 1,3 til 1,5 DA – altså store tomter! Tomtene er plassert slik i terrenget at det blir friarealer mellom grupper/felt av tomter. Området ligger i skrående terreng mot sør og sørvest, med god solgang og utsikt.",
+              "Området er regulert til fritidsbebyggelse, jfr. «Reguleringsplan for Lillehammer Sæter III». Planbestemmelsene er tilgjengelig hos tomteselger, eller kommunens nettside: <a href='http://lillehammer.kommune.no'>lillehammer.kommune.no</a>",
+              "Alle tomter leveres med vei, strøm, vann og avløp fram til tomtegrensene eller nær denne. Parkering på den enkelte eiendom.",
+              "Hyttestørrelsen kan være inntil 120m2 BRA + uthus inntil 30m2 BRA.",
+              "Alle tomtekjøpere har rett og plikt til å delta i felles hytteeierforening. Tomtene selges som selveiertomter, festekontrakter inngår ikke.",
+              "Det forutsettes at bygging på tomt igangsettes innen tre år fra tomtekjøp.",
+              "<b>KONTAKTPERSON TOMTER/VISNINGER:</b>",
+              "Arne Olav Berg",
+              "Tlf: 900 74 713",
+              "E-post: arolbe@gmail.com ",
+              "<b>TINDE HYTTER</b>",
+              "Noen av tomtene på området skal bebygges med hytter fra Tinde Hytter. de aktuelle tomtene er merket i prislisten. Tinde Hytter er ledende hytteleverandør i Sør-Norge med kontor og produksjon i Ringebu (7 mil nord for Lillehammer) og med lokal tilhøringhet til Lillehammer-regionen.",
+              "Tinde hytter tilbyr et stort antall hytter og de tilpasser gjerne sine løsninger til den aktuelle tomten eller dine behov.",
+              "Tinde Hytter tilbyr sine tjenester til avmerkede tomter så vel som til tomtekjøpere generelt.",
+              "Tinde Hytter har to visningshytter på Hellerudsletta Hytteutstilling (ved Exporama) i Skedsmo kommune utenfor Oslo, og de vil bygge egen visningshytte på Lillehammer Sæter III. Øvrige tomter er fri for egen utfoldelse, de ønsker ikke byggeplikt.",
+              "<b>KONTAKTPERSON TINDE HYTTER</b>",
+              "Jonny Jensvoll",
+              "Tlf: 916 53 980",
+              "E-post: jonny@tindehytter.no"
+            ],
+            "arrival": [
+              "Fra Lillehammer sentrum tar du av til Nordseter, godt merket flere steder.",
+              "Når du er kommet til Nordseter, kommer du til et vegkryss merket «Sjusøen». Fortsett rett fram gjennom svingen til venstre og du har skilt merket «Lillehammer Sæter» på venstre hånd, med avkjørsel ved dette skiltet – alt til venstre. Fortsett rett fram, til du har passert vannet på høyre side (Landtjernet).",
+              "Ta til venstre over broa og du er på området",
+              "Du kan også komme til Nordseter via Sjusjøen (helårsveg), eller fra Pellestova i Øyerfjellet (sommerveg)."
+            ],
+            "facts": [
+                "MOH: ca. 800",
+                "Tomtestørrelse: 1300–1500m2",
+                "Pris fra: 750 000,–",
+                "Til Lillehammer: 12 km",
+                "Nærmeste butikk: 6 km",
+                "Vann: Offentlig vann",
+                "Avløp: Offentlig avløp",
+                "Strøm: Jordkabel til tomtegrense"
+            ],
+            "priceInfo": [
+              "Det vises til egen prisliste for ledige tomter og tomtepriser.",
+              "Framføring av stikkledning for strøm fra tilkoblingspunkt til den enkelte hytte utføres av Eidisva Anlegg for kr 8 000,– pr enhet (pris pr. 08.10.15).",
+              "Tilkoblingsavgift til offentlig vann og avløp bestemmes av størrelse på hytte. Etter egen avtale med Lillehammer kommune skal tilkoblingavgift beregnes etter lav sats. Se kommunens nettside for eksakt informasjon: <a href='http://lillehammer.kommune.no'>lillehammer.kommune.no</a> For hytte på 100m2 utgjør dette pr. desember 2015 kr 23 000,– (mot 50 900,– med høy sats).",
+              "Felles kostnader til vedlikehold av veier, snøbrøyting. mm bestemmes av og fordeles gjennom hytteeierforeningen. Hytteeierforeningen er etablert.",
+              "Dokumentavgift og gebyr til tinglysning kommer i tillegg."
+            ],
+            "contactInfo":[
+              {
+                  "name": "TORBJØRN RANDEN",
+                  "tlf": "950 30 500",
+                  "email": "tr@tindeutvikling.no"
+              },
+              {
+                  "name": "ARNE OLAV",
+                  "tlf": "900 74 713",
+                  "email": "arolbe@gmail.com"
+              }
+            ]
         }
 
     },
@@ -163,11 +320,91 @@ var blabla = {
             "priceInfo": [
 
             ],
-            "contactInfo": {
-                "name": "",
-                "tlf": "",
-                "email": ""
-            }
+            "contactInfo":[
+              {
+                  "name": "",
+                  "tlf": "",
+                  "email": ""
+              }
+            ]
+        }
+
+    },
+    "grendnavn": {
+        "imgHeader": "",
+        "title": "",
+        "oneliner": "",
+        "description": [
+            "",
+            "",
+        ],
+        "icons": [
+            "",
+            ""
+        ],
+        "mapdata": [
+
+        ],
+        "information": {
+            "propertyInfo": [
+
+            ],
+            "arrival": [
+
+            ],
+            "facts": [
+                "",
+                ""
+            ],
+            "priceInfo": [
+
+            ],
+            "contactInfo":[
+              {
+                  "name": "",
+                  "tlf": "",
+                  "email": ""
+              }
+            ]
+        }
+
+    },
+    "grendnavn": {
+        "imgHeader": "",
+        "title": "",
+        "oneliner": "",
+        "description": [
+            "",
+            "",
+        ],
+        "icons": [
+            "",
+            ""
+        ],
+        "mapdata": [
+
+        ],
+        "information": {
+            "propertyInfo": [
+
+            ],
+            "arrival": [
+
+            ],
+            "facts": [
+                "",
+                ""
+            ],
+            "priceInfo": [
+
+            ],
+            "contactInfo":[
+              {
+                  "name": "",
+                  "tlf": "",
+                  "email": ""
+              }
+            ]
         }
 
     }
