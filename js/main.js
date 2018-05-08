93c7b89d28f7fdfa791610a1d47e97899906f302
ragnals <ragnals@stud.ntnu.no>
2018-05-08 12:12:19 +0200

    making folders for js

--- js/main.js
+++ js/main.js
@@ -1,60 +1,6 @@
-var filterItems = "";
+
 $(document).ready(function(){
-    $('.category_item').click(function(){
-        var checkboxes = $('[name=filterData]');
-        var selectedMisc = [];
-        var selectedAreas = [];
-        filterItems.addClass('hide');
-        $('.tagFilter').remove();
-
-        for (var i=0; i<checkboxes.length; i++) {
-            if(checkboxes[i].checked){
-                switch(checkboxes[i].value){
-                        case 'ringsaker':
-                            selectedAreas.push(checkboxes[i].value);
-                            break;
-                        case 'gudbrandsdalen':
-                            selectedAreas.push(checkboxes[i].value);
-                            break;
-                        case 'valdres':
-                            selectedAreas.push(checkboxes[i].value);
-                            break;
-                        case 'sør-trøndelag':
-                            selectedAreas.push(checkboxes[i].value);
-                            break;
-                        default:
-                            selectedMisc.push(checkboxes[i].value);
-                    }
-            }
-        }//Nå skal verdien for områder ligge i egen array, mens misc ligger i en annen.
-        if(selectedAreas.length > 0){
-            filterItems.addClass('hideArea');
-            for (var i=0; i<selectedAreas.length; i++){
-                $('.'+selectedAreas[i]).removeClass('hideArea');
-            }
-        }
-        for (var i=0; i<selectedMisc.length; i++) {
-            $('.'+selectedMisc[i]).removeClass('hide');
-        }
-        if(selectedMisc.length == 0){
-            filterItems.removeClass('hide');
-        }
-        if(selectedAreas.length == 0){
-            filterItems.removeClass('hideArea');
-        }
-        var checkedBoxes = selectedAreas.concat(selectedMisc);
-        for(var i=0; i<checkedBoxes.length; i++){
-          $('#filterTags').append('<div class="tagFilter">'+checkedBoxes[i]+'</div>');
-        }
-    });
-      $(".showCheckbox").on("click", function(e) {
-           $(this).nextAll(".hideCheckbox").toggle('fast');
-           var spanElement=e.target.firstElementChild;
-            $(spanElement).toggleClass("fas fa-chevron-up",true);
-            $(spanElement).toggleClass('fas fa-chevron-down',true);
-	     });
-
-    var myIndex = 0;
+    /*var myIndex = 0;
     carousel();
     function carousel() {
         var i;
@@ -123,7 +69,7 @@ $(document).ready(function(){
     });
     $("#fallBtn").click(function(){
       fallImages();
-    });
+    });*/
     //Calling function to initialize map
     //initMap();
     //initializing maps
