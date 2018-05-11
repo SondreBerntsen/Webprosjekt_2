c4de7cf70661de79bc3331f888b1c5af335dcc31
mads <mmoigaar@stud.ntnu.no>
2018-05-12 00:07:49 +0200

    fixed pullCardData for both home and area pages, however the CSS is acting weird on area

--- js/checkURL.js
+++ js/checkURL.js
@@ -1,5 +1,8 @@
 function checkURL(path){
   path = path.toLowerCase();
+  var PAData = hyttegrender;
+  var areaData = omrader;
+
 
   var homeRegex = path.match(/\/webprosjekt_2\/(home)\/?$/);
   var indexRegex = path.match(/\/webprosjekt_2\/(index\.html)\/?$/);
@@ -10,13 +13,14 @@ function checkURL(path){
   if(homeRegex || indexRegex || defaultRegex){
     console.log("Pretty sure HOME loads now, right?");
     $('#SPAContent').load('home.html', function() {
-      pullCardData();
+      for(i = 0; i < Object.keys(PAData).length; i++){
+        var pArea = Object.keys(PAData)[i];
+        pullCardData(pArea);
+      }
     });
   }
 
   if(propertyAreaRegex){
-
-    var PAData = hyttegrender;
     var areaIndex = propertyAreaRegex[1];
     var PAIndex = propertyAreaRegex[2];
 
@@ -41,7 +45,6 @@ function checkURL(path){
     !homeRegex &&
     areaRegex
   ){
-    var areaData = omrader;
     var areaIndex = areaRegex[1];
 
     //This prototype thing was taken from https://stackoverflow.com/questions/16576983/replace-multiple-characters-in-one-replace-call?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
@@ -63,6 +66,14 @@ function checkURL(path){
       console.log("Cool beans, load the area page");
       $('#SPAContent').load('/webprosjekt_2/areas/area.html', function(){
         loadContentArea(areaIndex);
+        for(i = 0; i < areaData[areaIndex].propertyAreas.length; i++){
+          var pArea = areaData[areaIndex].propertyAreas[i];
+          console.log(areaData[areaIndex].propertyAreas[i]);
+
+          pullCardData(pArea);
+          //CSS has to be changed hella for this page
+          //Also, adding classes is unneccessary without a filter function, and area name is redundant as it's on the area page.
+        }
       });
     }
   }
--- js/main.js
+++ js/main.js
@@ -1,73 +1,73 @@
 function loadContent(x){
-    var ourData = hyttegrender;
-    $('#imgHeader').attr("src", "../"+ourData[x].imgHeader)
-    $('#headerTitle').html(ourData[x].title);
-    $('#hgoneliner').html(ourData[x].oneliner);
+    var PAData = hyttegrender;
+    $('#imgHeader').attr("src", "../"+PAData[x].imgHeader)
+    $('#headerTitle').html(PAData[x].title);
+    $('#hgoneliner').html(PAData[x].oneliner);
 
-    for(i = 0; i < ourData[x].description.length; i++){
+    for(i = 0; i < PAData[x].description.length; i++){
         var templateElement = $('#pTemplate').clone();
 
         templateElement.removeAttr("id");
-        templateElement.html(ourData[x].description[i]);
+        templateElement.html(PAData[x].description[i]);
 
         $('#hgdescription').append(templateElement);
     }
 
-    for(i = 0; i < ourData[x].icons.length; i++){
+    for(i = 0; i < PAData[x].icons.length; i++){
         var templateElement = $('#iconTemplate').clone();
 
         templateElement.removeAttr("id");
-        templateElement.attr('src', '../images/icons/'+ourData[x].icons[i]+'.png');
-        templateElement.attr('title', ourData[x].icons[i]);
+        templateElement.attr('src', '../images/icons/'+PAData[x].icons[i]+'.png');
+        templateElement.attr('title', PAData[x].icons[i]);
 
         $('#icons').append(templateElement);
     }
-    initMapProperties(ourData[x]);
+    initMapProperties(PAData[x]);
 
-    for(i = 0; i < ourData[x].information.propertyInfo.length; i++){
+    for(i = 0; i < PAData[x].information.propertyInfo.length; i++){
       var templateElement = $('#pTemplate').clone();
 
       templateElement.removeAttr('id');
-      templateElement.html(ourData[x].information.propertyInfo[i]);
+      templateElement.html(PAData[x].information.propertyInfo[i]);
 
       $('#propertyInfo').append(templateElement);
     }
 
 
-    for(i = 0; i < ourData[x].information.arrival.length; i++){
+    for(i = 0; i < PAData[x].information.arrival.length; i++){
       var templateElement = $('#pTemplate').clone();
 
       templateElement.removeAttr('id');
-      templateElement.html(ourData[x].information.arrival[i]);
+      templateElement.html(PAData[x].information.arrival[i]);
 
       $('#arrival').append(templateElement);
     }
 
-    for(i = 0; i < ourData[x].information.facts.length; i++){
+    for(i = 0; i < PAData[x].information.facts.length; i++){
       var templateElement = $('#pTemplate').clone();
 
       templateElement.removeAttr('id');
-      templateElement.html(ourData[x].information.facts[i]);
+      templateElement.html(PAData[x].information.facts[i]);
 
       $('#facts').append(templateElement);
     }
 
-    for(i = 0; i < ourData[x].information.priceInfo.length; i++){
+    for(i = 0; i < PAData[x].information.priceInfo.length; i++){
       var templateElement = $('#pTemplate').clone();
 
       templateElement.removeAttr('id');
-      templateElement.html(ourData[x].information.priceInfo[i]);
+      templateElement.html(PAData[x].information.priceInfo[i]);
 
       $('#priceInfo').append(templateElement);
     }
 
-    for(i = 0; i < ourData[x].information.contactInfo.length; i++){
+    for(i = 0; i < PAData[x].information.contactInfo.length; i++){
         var templateElement = $('#contactTemplate').clone();
 
         templateElement.removeAttr('id');
-        templateElement.find('.contactName').html(ourData[x].information.contactInfo[i].name);
-        templateElement.find('.fa-phone').after(ourData[x].information.contactInfo[i].tlf);
-        templateElement.find('.fa-envelope').after(ourData[x].information.contactInfo[i].email);
+        templateElement.find('.contactName').html(PAData[x].information.contactInfo[i].name);
+        templateElement.find('.fa-phone').after(PAData[x].information.contactInfo[i].tlf);
+        templateElement.find('.fa-envelope').after(PAData[x].information.contactInfo[i].email);
 
         $('#contactInfo').append(templateElement.html());
     }
@@ -88,7 +88,7 @@ function initMapArea(areaData){
 
   for (i = 0; i < areaData.mapData.markers.length; i++){
     var markerPosition = areaData.mapData.markers[i].position;
-    console.log(markerPosition);
+    //console.log(markerPosition);
     var label= areaData.mapData.markers[i].propertyAreaName;
     var marker = new google.maps.Marker({
       position: markerPosition,
@@ -120,7 +120,7 @@ function scrollToTable(){
 }
 function loadNavItems(){
   //$('#templates').load('/webprosjekt_2/includeFiles/templates.html'); //This is pointless as they can't be cloned from there anyway. I think the templates must be file separated and loaded to their respective target containers once the page loads. Then again it works on propertyArea, what the fuck.
-  var ourData = hyttegrender;
+  var PAData = hyttegrender;
   var areaData = omrader;
 
   for(i = 0; i < Object.keys(areaData).length; i++){
@@ -163,26 +163,22 @@ function initMapProperties(PA){
   }
 }
 
-function pullCardData(){ //needs parameter to be used for both index and area pages
+function pullCardData(pArea){ //needs parameter to be used for both index and area pages
 
-    var ourData = hyttegrender;
+  var PAData = hyttegrender;
 
-    for(i = 0; i < Object.keys(ourData).length; i++){
-      temp = Object.keys(ourData)[i];
+  var templateElement = $('#cardTemplate').clone();
+  templateElement.removeAttr("id");
+  templateElement.addClass(PAData[pArea].area);
+  templateElement.addClass(PAData[pArea].icons.join(" "));
+  templateElement.find("a").attr('href', PAData[pArea].area.toLowerCase()+'/'+PAData[pArea].name);
+  templateElement.find("img").attr('src', 'images/thumbnailsFilter/'+pArea+'.jpeg');
+  templateElement.find(".availabilityText").html(PAData[pArea].cardInfo.availability);
+  templateElement.find(".price").html(PAData[pArea].cardInfo.price);
+  templateElement.find(".card-title").html(PAData[pArea].title);
+  templateElement.find(".areaName").html(PAData[pArea].area);
+  templateElement.find(".card-text").html(PAData[pArea].oneliner);
 
-      var templateElement = $('#cardTemplate').clone();
-      templateElement.removeAttr("id");
-      templateElement.addClass(ourData[temp].area);
-      templateElement.addClass(ourData[temp].icons.join(" "));
-      templateElement.find("a").attr('href', ourData[temp].area.toLowerCase()+'/'+ourData[temp].name);
-      templateElement.find("img").attr('src', 'images/thumbnailsFilter/'+temp+'.jpeg');
-      templateElement.find(".availabilityText").html(ourData[temp].cardInfo.availability);
-      templateElement.find(".price").html(ourData[temp].cardInfo.price);
-      templateElement.find(".card-title").html(ourData[temp].title);
-      templateElement.find(".areaName").html(ourData[temp].area);
-      templateElement.find(".card-text").html(ourData[temp].oneliner);
-
-      $('#cardContainer').append(templateElement);
-      filterItems = $('.filter_item');
-    }
+  $('#cardContainer').append(templateElement);
+  filterItems = $('.filter_item');
 }
